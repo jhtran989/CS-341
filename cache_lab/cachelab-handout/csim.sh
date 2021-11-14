76af7c514ba78f6287aa78ede5967dd44b671c82
John Tran <jtran989@unm.edu>
2021-11-13 21:20:25 -0700

    test suspicion that the request sizes don't matter (one address represent one byte...)

--- cache_lab/cachelab-handout/csim.sh
+++ cache_lab/cachelab-handout/csim.sh
@@ -8,7 +8,7 @@ gcc -o csim_test main.c
 
 # test different E values (s and b stay the same at 4 for easy checking -- 4
 # bits for s and b means one hexadecimal place for each)
-./csim_test -v -s 4 -E 2 -b 4 -t traces/yi.trace
+#./csim_test -v -s 4 -E 2 -b 4 -t traces/yi.trace
 #./csim_test -v -s 4 -E 4 -b 4 -t traces/yi.trace
 #./csim_test -v -s 4 -E 8 -b 4 -t traces/yi.trace
 #./csim_test -v -s 4 -E 16 -b 4 -t traces/yi.trace
@@ -19,6 +19,8 @@ gcc -o csim_test main.c
 #./csim -v -s 1 -E 1 -b 1 -t traces/yi2.trace > test_case_personal_1.txt
 #./csim-ref -v -s 1 -E 1 -b 1 -t traces/yi2.trace > test_case_ref_1.txt
 #diff test_case_personal_1.txt test_case_ref_1.txt > test_case_output.txt
+
+./csim_test -v -s 2 -E 1 -b 4 -t traces/dave.trace
 
 # find differences (verbose)
 
